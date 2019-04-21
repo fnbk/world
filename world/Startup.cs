fc26bcd4ec7aa0dccc870a069713c066215a6573
Florian Boehmak <Florian.Boehmak@plan-b-gmbh.com>
2019-04-21 20:21:46 +0200

    print hello world

--- world/Startup.cs
+++ world/Startup.cs
@@ -38,6 +38,11 @@ namespace world
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello, World!");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
